0880ddc4f94eddcf392c343e9bd262322d9a6672
davidkatz <15Dkatz@shcp.edu>
2016-08-14 15:07:02 -0700

    fix  unexpected token in app.jsx error

--- typeDash/client/app.jsx
+++ typeDash/client/app.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 let socket = io.connect();
 
 let App = React.createClass({
-  // componentDidMount() {
-  //   console.log('chatApp connected');
-  //   socket.on('init', this._initialize);
-  // },
-  //
-  // _initialize(data) {
-  //   let {users, name} = data;
-  //   console.log('Should set the following to state:');
-  //   console.log('users', users);
-  //   console.log('name', name);
-  // },
+  componentDidMount() {
+    console.log('chatApp connected');
+    socket.on('init', this._initialize);
+  },
+
+  _initialize(data) {
+    let {users, name} = data;
+    console.log('Should set the following to state:');
+    console.log('users', users);
+    console.log('name', name);
+  },
 
   render() {
     return (
